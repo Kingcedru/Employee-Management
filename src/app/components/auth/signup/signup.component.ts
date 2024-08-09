223d016deb6d9520906b1ff82c26ab8922039828
Kingcedru <byishimocedrick@gmail.com>
2024-08-09 11:03:00 +0200

    getting Form data

--- src/app/components/auth/signup/signup.component.ts
+++ src/app/components/auth/signup/signup.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { InputFieldComponent } from '../../input-field/input-field.component';
 
 @Component({
   selector: 'app-signup',
@@ -9,8 +11,19 @@ export class SignupComponent implements OnInit {
   placeholderPass: string = '********';
   placeholderUser: string = 'Username';
   inputTypes: boolean = true;
+  signupForm!: NgForm;
+
+  @ViewChild('username') username!: InputFieldComponent;
+  @ViewChild('password') password!: InputFieldComponent;
+  @ViewChild('repassword') repassword!: InputFieldComponent;
 
   constructor() {}
 
   ngOnInit() {}
+
+  onSubmit(signupForm: NgForm) {
+    console.log(this.username.value);
+    console.log(this.password.value);
+    console.log(this.repassword.value);
+  }
 }
